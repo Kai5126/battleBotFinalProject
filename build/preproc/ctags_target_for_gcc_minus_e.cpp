3c7726727929d824a1215e848f003efa3231b8b8
Kai5126 <moonsplashyt@gmail.com>
2022-03-22 12:48:53 -0400

    first commit

---
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,10 @@
+# 1 "c:\\Users\\moons\\Desktop\\Arduino\\battleBotFinalProject\\sketch.ino"
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ build/sketch/sketch.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "c:\\Users\\moons\\Desktop\\Arduino\\battleBotFinalProject\\sketch.ino"
+#line 1 "c:\\Users\\moons\\Desktop\\Arduino\\battleBotFinalProject\\sketch.ino"
+void setup();
+#line 6 "c:\\Users\\moons\\Desktop\\Arduino\\battleBotFinalProject\\sketch.ino"
+void loop();
+#line 1 "c:\\Users\\moons\\Desktop\\Arduino\\battleBotFinalProject\\sketch.ino"
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
